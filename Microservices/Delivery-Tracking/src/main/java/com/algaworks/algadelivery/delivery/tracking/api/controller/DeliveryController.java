e592c6b77acea669fdc28769864df4839c7313c7
Victor Hugo <victorhugopb8@gmail.com>
2026-01-16 18:12:37 -0300

    6.4. Implementando consulta de Delivery

--- Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/api/controller/DeliveryController.java
+++ Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/api/controller/DeliveryController.java
@@ -2,11 +2,15 @@ package com.algaworks.algadelivery.delivery.tracking.api.controller;
 
 import com.algaworks.algadelivery.delivery.tracking.api.model.DeliveryInput;
 import com.algaworks.algadelivery.delivery.tracking.domain.model.Delivery;
+import com.algaworks.algadelivery.delivery.tracking.domain.repository.DeliveryRepository;
 import com.algaworks.algadelivery.delivery.tracking.domain.service.DeliveryPreparationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -16,6 +20,8 @@ import java.util.UUID;
 public class DeliveryController {
 
     private final DeliveryPreparationService deliveryPreparationService;
+
+    private final DeliveryRepository deliveryRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -29,4 +35,15 @@ public class DeliveryController {
     public Delivery edit(@PathVariable UUID deliveryId, @RequestBody @Valid DeliveryInput request) {
         return deliveryPreparationService.edit(request, deliveryId);
     }
+
+    @GetMapping
+    public PagedModel<Delivery> findAll(Pageable pageable) {
+        return new PagedModel<>(deliveryRepository.findAll(pageable));
+    }
+
+    @GetMapping("/{deliveryId}")
+    public Delivery findById(@PathVariable UUID deliveryId) {
+        return deliveryRepository.findById(deliveryId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
 }
