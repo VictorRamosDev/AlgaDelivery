78276f4deb00fc4ea14dffef66934090e06ec01b
Victor Hugo <victorhugopb8@gmail.com>
2026-01-16 17:51:24 -0300

    6.3. Domain Services

--- Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/api/controller/DeliveryController.java
+++ Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/api/controller/DeliveryController.java
@@ -2,26 +2,31 @@ package com.algaworks.algadelivery.delivery.tracking.api.controller;
 
 import com.algaworks.algadelivery.delivery.tracking.api.model.DeliveryInput;
 import com.algaworks.algadelivery.delivery.tracking.domain.model.Delivery;
+import com.algaworks.algadelivery.delivery.tracking.domain.service.DeliveryPreparationService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/deliveries")
 public class DeliveryController {
+
+    private final DeliveryPreparationService deliveryPreparationService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Delivery draft(@RequestBody @Valid DeliveryInput request) {
         System.out.println("Validando criação de rascunho de entrega...");
         System.out.println("Funcionando.");
-        return null;
+        return deliveryPreparationService.draft(request);
     }
 
     @PutMapping("/{deliveryId}")
     public Delivery edit(@PathVariable UUID deliveryId, @RequestBody @Valid DeliveryInput request) {
-        return null;
+        return deliveryPreparationService.edit(request, deliveryId);
     }
 }
---
+++ Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/service/DeliveryPreparationService.java
@@ -0,0 +1,82 @@
+package com.algaworks.algadelivery.delivery.tracking.domain.service;
+
+import com.algaworks.algadelivery.delivery.tracking.api.model.ContactPointInput;
+import com.algaworks.algadelivery.delivery.tracking.api.model.DeliveryInput;
+import com.algaworks.algadelivery.delivery.tracking.api.model.ItemInput;
+import com.algaworks.algadelivery.delivery.tracking.domain.exception.DomainException;
+import com.algaworks.algadelivery.delivery.tracking.domain.model.ContactPoint;
+import com.algaworks.algadelivery.delivery.tracking.domain.model.Delivery;
+import com.algaworks.algadelivery.delivery.tracking.domain.repository.DeliveryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class DeliveryPreparationService {
+
+    private final DeliveryRepository deliveryRepository;
+
+    @Transactional
+    public Delivery draft(DeliveryInput input) {
+        Delivery delivery = Delivery.draft();
+        this.handlePreparation(delivery, input);
+
+        return deliveryRepository.save(delivery);
+    }
+
+    @Transactional
+    public Delivery edit(DeliveryInput input, UUID deliveryId) {
+        Delivery delivery = deliveryRepository.findById(deliveryId).orElseThrow(() -> new DomainException());
+        delivery.removeItems(); //Adicionado apenas para facilitar, mas o ideal seria um de-para para validar quais items foram alterados
+        handlePreparation(delivery, input);
+
+        return deliveryRepository.save(delivery);
+    }
+
+    public void handlePreparation(Delivery delivery, DeliveryInput input) {
+        ContactPointInput senderInput = input.getSender();
+        ContactPointInput recipientInput = input.getRecipient();
+
+        ContactPoint sender = ContactPoint.builder()
+                .zipCode(senderInput.getZipCode())
+                .street(senderInput.getStreet())
+                .number(senderInput.getNumber())
+                .complement(senderInput.getComplement())
+                .name(senderInput.getName())
+                .phone(senderInput.getPhone())
+                .build();
+
+        ContactPoint recipient = ContactPoint.builder()
+                .zipCode(recipientInput.getZipCode())
+                .street(recipientInput.getStreet())
+                .number(recipientInput.getNumber())
+                .complement(recipientInput.getComplement())
+                .name(recipientInput.getName())
+                .phone(recipientInput.getPhone())
+                .build();
+
+        Duration expectedDeliveryTime = Duration.ofHours(3);
+        BigDecimal distanceFee = BigDecimal.valueOf(15.0);
+        BigDecimal payout = BigDecimal.TEN;
+
+        var preparationDetails = Delivery.PreparationDetails.builder()
+                .sender(sender)
+                .recipient(recipient)
+                .distanceFee(distanceFee)
+                .courierPayout(payout)
+                .expectedDeliveryTime(expectedDeliveryTime)
+                .build();
+
+        delivery.editPreparationDetails(preparationDetails);
+
+        for (ItemInput itemInput : input.getItems()) {
+            delivery.addItem(itemInput.getName(), itemInput.getQuantity());
+        }
+
+    }
+}
